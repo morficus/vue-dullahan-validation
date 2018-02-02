342523149c5eab72a0909babdecf309521278e15
Maurice Williams <morficus@gmail.com>
2018-02-01 22:28:00 -0500

    adding tests for helper functions
    * updating helper functions to guard against null and undefied values
    * adding tests

--- src/helpers/compareArray.js
+++ src/helpers/compareArray.js
@@ -1,5 +1,4 @@
 import compareObject from './compareObject';
-const EMPTY = 0;
 
 /**
  * Check if the two given arrays are the same by doing a comparison of their individuals items.
@@ -10,34 +9,38 @@ const EMPTY = 0;
  * @returns {boolean} True if they are the same, otherwise False
  */
 export default function compareArray (dataValue, partnerValue) {
-    let isValid = true;
+    let isValid = false;
 
-    // find miss-matched values.
-    // the "gap" here is that if one list is longer than the other... then the miss-match might not be detected.
-    // but there is a check for that later on.
-    const missMatchedValues = dataValue.filter((val, index) => {
-        let missMatchVal = undefined;
+    if (dataValue && partnerValue) {
+        // find miss-matched values.
+        // the "gap" here is that if one list is longer than the other... then the miss-match might not be detected.
+        // but there is a check for that later on.
+        const missMatchedValues = dataValue.filter((val, index) => {
+            let missMatchVal = undefined;
 
-        if (Array.isArray(val) || Array.isArray(partnerValue[index])) {
-            // if values at the given index are arrays... go in and check those too
-            // because the "partner value" could be shorter than the value its self... we set a fallback value of empty
-            // array.
-            missMatchVal = !compareArray(val, partnerValue[index] || []);
-        } else if (val instanceof Object || partnerValue[index] instanceof Object) {
-            missMatchVal = !compareObject(val, partnerValue[index]);
-        } else {
-            missMatchVal = val !== partnerValue[index];
-        }
+            if (Array.isArray(val) || Array.isArray(partnerValue[index])) {
+                // if values at the given index are arrays... go in and check those too
+                // because the "partner value" could be shorter than the value its self... we set a fallback value of
+                // empty array.
+                missMatchVal = !compareArray(val, partnerValue[index] || []);
+            } else if (val instanceof Object || partnerValue[index] instanceof Object) {
+                missMatchVal = !compareObject(val, partnerValue[index]);
+            } else {
+                missMatchVal = val !== partnerValue[index];
+            }
 
-        return missMatchVal;
-    });
+            return missMatchVal;
+        });
 
-    if (dataValue.length !== partnerValue.length) {
         // must be the same length
-        isValid = false;
-    } else if (missMatchedValues.length !== EMPTY) {
-        // must have the same content in the same order
-        isValid = false;
+        if (dataValue.length !== partnerValue.length ||
+            // must have the same content in the same order
+            missMatchedValues.length) {
+
+            isValid = false;
+        } else {
+            isValid = true;
+        }
     }
 
     return isValid;
--- src/helpers/compareObject.js
+++ src/helpers/compareObject.js
@@ -8,15 +8,25 @@ import compareArray from './compareArray';
  * @returns {boolean} True if they are the same, otherwise False
  */
 export default function compareObject (dataValue, partnerValue) {
-    // generate an array of keys and an array of values
-    const dataKeys = Object.keys(dataValue),
-        dataValues = Object.values(dataValue),
-        partnerKeys = Object.keys(partnerValue),
-        partnerValues = Object.values(partnerValue),
 
-        // now that we have an array of strings... just treat them as normal arrays :-)
-        doKeysMatch = compareArray(dataKeys, partnerKeys),
-        doValuesMatch = compareArray(dataValues, partnerValues);
+    let isValid = false;
 
-    return doKeysMatch && doValuesMatch;
+    if (dataValue && partnerValue) {
+        // generate an array of keys and an array of values
+        // sorting so that order of keys will not influence the result
+        const dataKeys = Object.keys(dataValue).sort(),
+            dataValues = Object.values(dataValue).sort(),
+            partnerKeys = Object.keys(partnerValue).sort(),
+            partnerValues = Object.values(partnerValue).sort(),
+
+            // now that we have an array of strings... just treat them as normal arrays :-)
+            doKeysMatch = compareArray(dataKeys, partnerKeys),
+            doValuesMatch = compareArray(dataValues, partnerValues);
+
+        isValid = doKeysMatch && doValuesMatch;
+    }
+
+    return isValid;
+
+
 }
